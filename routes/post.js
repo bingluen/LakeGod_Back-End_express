138f9a30aaa33b8a6da51fe29f7a2910aa72bef4
Erickson Juang <a820224@gmail.com>
2015-05-04 01:34:51 +0800

    separate post router

--- routes/post.js
+++ routes/post.js
@@ -6,8 +6,13 @@ var post = require('../modules/post');
 /* Router setting */
 
 router.post('/addPost', post.addPost);
-router.get('/getPost/', post.getPost);
-router.get('/getPost/:id', post.getPost);
+router.get('/getPost/id/:id', post.getPost.ByID);
+router.get('/getPost/title/:key/:type', post.getPost.ByTitle);
+router.get('/getPost/tag/:key/:type', post.getPost.ByTag);
+router.get('/getPost/user/:key/:type', post.getPost.ByUser);
+//router.get('/getPost/occure_time/:start-:end/:type', post.getPost.ByOccureTime);
+//router.get('/getPost/upload_time/:start-:end/:type', post.getPost.ByUploadTime);
+
 router.post('/deletePost', post.deletePost);
 router.post('/updatePost', post.updatePost);
 
