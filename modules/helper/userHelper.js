a6bae976da9583bdd27f558496c4e39dd4e11d0b
Erickson Juang <a820224@gmail.com>
2015-05-06 22:24:29 +0800

    finish get user name

---
+++ modules/helper/userHelper.js
@@ -0,0 +1,19 @@
+var database = require('../database');
+
+var getUserName = function(userid, next) {
+    database.query('SELECT `name` FROM `user` WHERE `id` = ?', userid, function(error, row, fields) {
+        if (error) {
+            res.status(500).json({
+                status_messages: 'Internal error',
+                status_code: 500
+            });
+            console.log('ERROR: userHelper.getUserName:' + error);
+        }
+        if (row.length == 1)
+            next(row[0].name);
+        else
+            next(undefined);
+    });
+};
+
+module.exports.getUserName = getUserName;
