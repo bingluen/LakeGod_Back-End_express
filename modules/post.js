9edbd5bd0db9520acbf719284e318f17f6949208
Erickson Juang <a820224@gmail.com>
2015-06-18 17:33:48 +0800

    modify post table

--- modules/post.js
+++ modules/post.js
@@ -11,17 +11,18 @@ var appConfig = require('../appConfig.json')
 getPost.List = function(req, res, next) {
     var queryStatement = '';
     var parameter = [];
-    var pages = req.params.pages > 0 ? (req.params.pages - 1) * config.post.postNumOfList : 0;
+    var pages = req.params.pages > 0 ? (req.params.pages - 1) * appConfig.post.postNumOfList : 0;
 
     if (req.params.type.toLowerCase() === 'all') {
-        queryStatement = 'SELECT * FROM `post` ORDER BY `upload_time` LIMIT ?, ?;';
+        //
+        queryStatement = 'SELECT result.*, user.name as author_name FROM (SELECT * FROM `post` ORDER BY `upload_time` LIMIT ?, ?) result, user Where result.author_id = user.id;';
         parameter.push(pages);
-        parameter.push(config.post.postNumOfList);
+        parameter.push(appConfig.post.postNumOfList);
     } else {
-        queryStatement = 'SELECT * FROM `post` WHERE `type` = ? ORDER BY `upload_time` LIMIT ?, ?;';
+        queryStatement = 'SELECT result.*, user.name as author_name FROM (SELECT * FROM `post` WHERE `type` = ? ORDER BY `upload_time` LIMIT ?, ?) result, user Where result.author_id = user.id;';
         parameter.push(req.params.type);
         parameter.push(pages);
-        parameter.push(config.post.postNumOfList);
+        parameter.push(appConfig.post.postNumOfList);
     }
 
 
@@ -49,7 +50,8 @@ getPost.List = function(req, res, next) {
 };
 
 getPost.ByID = function(req, res, next) {
-    database.query('SELECT * FROM `post` WHERE id = ?', req.params.id, function(error, row, fields) {
+
+    database.query('SELECT result.*, user.name as author_name FROM (SELECT * FROM `post` WHERE id = ?) result, user Where result.author_id = user.id;', req.params.id, function(error, row, fields) {
         if (error) {
             res.status(500).json({
                 status_messages: 'Internal error',
@@ -77,10 +79,10 @@ getPost.ByTitle = function(req, res, next) {
     var queryStatement = '';
     var parameter = [];
     if (req.params.type === 'all') {
-        queryStatement = 'SELECT * FROM `post` WHERE `title` LIKE ?;';
+        queryStatement = 'SELECT result.*, user.name as author_name FROM (SELECT * FROM `post` WHERE `title` LIKE ?) result, user Where result.author_id = user.id;';
         parameter.push('%' + req.params.key + '%');
     } else {
-        queryStatement = 'SELECT * FROM `post` WHERE `title` LIKE ? AND `type` = ?;';
+        queryStatement = 'SELECT result.*, user.name as author_name FROM (SELECT * FROM `post` WHERE `title` LIKE ? AND `type` = ?;) result, user Where result.author_id = user.id;';
         parameter.push('%' + req.params.key + '%')
         parameter.push(req.params.type);
     }
@@ -113,10 +115,10 @@ getPost.ByTag = function(req, res, next) {
     var queryStatement = '';
     var parameter = '';
     if (req.params.type === 'all') {
-        queryStatement = 'SELECT * FROM `post` WHERE `tag` LIKE ?;';
+        queryStatement = 'SELECT result.*, user.name as author_name FROM (SELECT * FROM `post` WHERE `tag` LIKE ?) result, user Where result.author_id = user.id;';
         parameter = '%' + req.params.key + '%';
     } else {
-        queryStatement = 'SELECT * FROM `post` WHERE `tag` LIKE ? AND `type` = ?;';
+        queryStatement = 'SELECT result.*, user.name as author_name FROM (SELECT * FROM `post` WHERE `tag` LIKE ? AND `type` = ?) result, user Where result.author_id = user.id;';
         parameter = new Array('%' + req.params.key + '%', req.params.type);
     }
     database.query(queryStatement, parameter, function(error, row, fields) {
@@ -147,11 +149,11 @@ getPost.ByUser = function(req, res, next) {
     var queryStatement = '';
     var parameter = '';
     if (req.params.type === 'all') {
-        queryStatement = 'SELECT * FROM `post` WHERE `author_name` LIKE ? OR `author_id` = ?;';
-        parameter = new Array('%' + req.params.key + '%', req.params.key);
+        queryStatement = 'SELECT * FROM (SELECT post.*, user.name as author_name FROM user, post WHERE post.author_id = user.id) result WHERE result.author_name LIKE ?;';
+        parameter = new Array('%' + req.params.key + '%');
     } else {
-        queryStatement = 'SELECT * FROM `post` WHERE (`author_name` LIKE ? OR `author_id` = ?) AND `type` = ?;';
-        parameter = new Array('%' + req.params.key + '%', req.params.key, req.params.type);
+        queryStatement = 'SELECT * FROM (SELECT post.*, user.name as author_name FROM user, post WHERE post.author_id = user.id) result WHERE result.author_name LIKE ? AND result.type = ?;';
+        parameter = new Array('%' + req.params.key + '%', req.params.type);
     }
     database.query(queryStatement, parameter, function(error, row, fields) {
         if (error) {
@@ -178,14 +180,14 @@ getPost.ByUser = function(req, res, next) {
 }
 
 
-getPost.ByLocation = function (req, res, next) {
+getPost.ByLocation = function(req, res, next) {
     var queryStatement = '';
     var parameter = '';
     if (req.params.type === 'all') {
-        queryStatement = 'SELECT * FROM `post` WHERE `location` LIKE ? ;';
+        queryStatement = 'SELECT result.*, user.name as author_name FROM (SELECT * FROM `post` WHERE `location` LIKE ? ) result, user Where result.author_id = user.id;';
         parameter = new Array('%' + req.params.key + '%');
     } else {
-        queryStatement = 'SELECT * FROM `post` WHERE `location` LIKE ? AND `type` = ?;';
+        queryStatement = 'SELECT result.*, user.name as author_name FROM (SELECT * FROM `post` WHERE `location` LIKE ? AND `type` = ?) result, user Where result.author_id = user.id;';
         parameter = new Array('%' + req.params.key + '%', req.params.type);
     }
     database.query(queryStatement, parameter, function(error, row, fields) {
@@ -208,7 +210,7 @@ getPost.ByLocation = function (req, res, next) {
                 status_code: 404
             })
         }
-    });    
+    });
 }
 
 var addPost = function(req, res, next) {
@@ -218,12 +220,26 @@ var addPost = function(req, res, next) {
 
     /* 驗證FB token是否合法 */
     var token = data['fb_token'];
+    var facebook = require('./facebook');
 
     /* 驗證FB_id是否已註冊 */
     var userHelper = require('./helper/userHelper');
 
+    facebook({
+        fbtoken: data['fb_token'],
+        LoginCallback: function(body) {
+            if (body.vaild)
+                userHelper.getUserName(body.data.id, post);
+            else {
+                res.status(412).json({
+                    status_messages: 'facebook token fail.',
+                    status_code: 412
+                });
+            }
+        }
+    });
 
-    userHelper.getUserName(dataFilter(data['fb_user_id']), function(username) {
+    var post = function(username, user_id) {
         /* 不合法直接退貨w */
         if (!(typeof username !== 'undefined')) {
             res.status(412).json({
@@ -237,14 +253,13 @@ var addPost = function(req, res, next) {
             parameter.push(dataFilter(data['description']));
             parameter.push(dataFilter(data['photos']));
             parameter.push(dataFilter(data['fb_user_id']));
-            parameter.push(dataFilter(username));
             parameter.push(dataFilter(data['type']));
             parameter.push(dataFilter(data['location']));
             parameter.push(dataFilter(data['map_lat']));
             parameter.push(dataFilter(data['map_log']));
             parameter.push(dataFilter(data['occure_time']));
 
-            var queryStatement = 'INSERT INTO `post`(`title`, `tag`, `description`, `photos`, `author_id`, `author_name`, `type`, `location`, `map_lat`, `map_lng`, `occure_time`) VALUE( ? , ? , ? , ? , ? , ? , ? , ? , ? , ?, ? );';
+            var queryStatement = 'INSERT INTO `post`(`title`, `tag`, `description`, `photos`, `author_id`, `type`, `location`, `map_lat`, `map_lng`, `occure_time`) VALUE( ? , ? , ? , ? , ? , ? , ? , ? , ?, ? );';
 
             /* 儲存到資料庫 */
             database.query(queryStatement, parameter, function(error, row, fields) {
@@ -262,10 +277,9 @@ var addPost = function(req, res, next) {
                 }
             });
         }
-    });
+    }
 };
 
 
 module.exports.getPost = getPost;
 module.exports.addPost = addPost;
-
